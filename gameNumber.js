3f2e3cd5b6206d723faeaeade458cfd09505dd87
Hasan-Kamran <153837119+Hasan-Kamran@users.noreply.github.com>
2023-12-15 16:08:09 +0000

    guess the game number prompt

---
+++ gameNumber.js
@@ -0,0 +1,8 @@
+let gameNum = 45;
+let userNum = prompt("Guess the game number : ");
+
+while(userNum != gameNum) {
+    userNum = prompt("your entered wrong number : ");
+
+}
+console.log("Congratulations, your number is absolutely correct : ");
